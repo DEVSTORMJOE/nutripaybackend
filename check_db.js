5de50d988e4673c8ff90389404d66a4ac86de122
frankosparks-prog <mainafrank400@gmail.com>
2026-06-09 21:44:26 +0300

    chore: delete unused test and diagnostic files

--- check_db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const User = require("./models/User");
-
-mongoose.connect(process.env.MONGO_URI || "mongodb+srv://mainafrank400_db_user:N7Og3gx1cvVI0AnS@nutri.xyyf1zm.mongodb.net/?appName=nutri").then(async () => {
-  const sponsors = await User.find({ role: "sponsor" });
-  console.log("Sponsors found:", sponsors.length);
-  sponsors.forEach(s => console.log(s.email, s.name, s.createdAt));
-  process.exit(0);
-}).catch(console.error);
